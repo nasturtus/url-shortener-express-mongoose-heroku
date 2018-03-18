45f711c7f9b323582646e0912834165eaea7df29
nasturtus <nasturtus@gmail.com>
2018-03-19 01:56:19 +0800

    Add error handler to app.js and add dependencies to package.json

--- app.js
+++ app.js
@@ -14,21 +14,28 @@ app.use(bodyParser.json());
 let index = require("./routes/index");
 let shortenUrl = require("./routes/shortenUrl");
 let expandUrl = require("./routes/expandUrl");
-let allUrls = require('./routes/allUrls')
+let allUrls = require("./routes/allUrls");
 
 // routes
 app.use("/", index);
-app.use('/all', allUrls)
+app.use("/all", allUrls);
 app.use("/shortenurl", shortenUrl);
 app.use("/expandurl", expandUrl);
 
 // connect to database
 const dbUrl = process.env.MONGODB_URI;
-mongoose.connect(dbUrl)
+mongoose.connect(dbUrl);
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("Connected to mongodb...")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to mongodb...");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // server start
